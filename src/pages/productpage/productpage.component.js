71565fe25e1668812b1868ef12098ada54cea251
TomikBoi <toms.kedo@gmail.com>
2021-12-10 21:34:08 +0200

    Added product info side with little styling till pricing

--- src/pages/productpage/productpage.component.js
+++ src/pages/productpage/productpage.component.js
@@ -1,10 +1,13 @@
 import React from "react";
-
-import "./productpage.styles.scss";
+import { connect } from "react-redux";
+import { addAttribute } from "../../redux/cart/cart.actions";
+import { getCurrencySymbol } from "../../helper/getCurrencySymbol";
+import "./productpage.styles.scss"
 
 class ProductPage extends React.Component {
   state = {
-    clickedImage: this.props.product.gallery[0]
+    clickedImage: this.props.product.gallery[0],
+    selecetedAttributes: {}
   }
 
   setImage = (img) => {
@@ -13,8 +16,22 @@ class ProductPage extends React.Component {
     })
   }
 
+  handleChange = e => {
+    const { name, value } = e.target;
+
+    this.setState({
+      
+     selecetedAttributes: {... this.state.selecetedAttributes,[name]: value }
+    });
+  };
+
   render() {
-    const { product } = this.props;
+    const { product, addAttribute, currency } = this.props;
+
+    const price = product.prices
+      .filter((item) => item.currency === currency)
+      .map((filteredItem) => filteredItem.amount);
+
 
     return (
       <div className="product-container">
@@ -26,10 +43,59 @@ class ProductPage extends React.Component {
           </div>
           <div className="product-gallery-big"><img className='product-gallery-big-image' src={this.state.clickedImage} /></div>
         </div>
-        <div className="product-info"></div>
+        <div className="product-info">
+        <p className='product-info-name'>{product.brand}</p>
+        <p className='product-info-brand'>{product.name}</p>
+
+        {product.attributes.map((attribute) => (
+          <form name={attribute.id} key={attribute.id} className="attribute-form">
+          <p className='attribute-name'>{attribute.id}:</p>
+            <div className="attribute-btn">
+              {attribute.items.map((item) => (
+                <div key={item.id}>
+                  <input
+                    type="radio"
+                    name={attribute.id}
+                    id={`${item.id}-${attribute.id}-${product.id}`}
+                    value={item.value}
+                    checked={this.state.selecetedAttributes[`${attribute.id}`] === item.value}
+                    onChange={(e) =>
+                      this.handleChange(e)
+                    }
+                  />
+
+                  <label
+                    className={`attribute-radio-label attribute-radio-label-${attribute.type} `} 
+                    style={ attribute.type === "swatch" ? { backgroundColor: `${item.value}` } : null}
+                    htmlFor={`${item.id}-${attribute.id}-${product.id}`}
+                  >
+                    {attribute.type === "swatch" ? null : item.value}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </form>
+        ))}
+
+        <p className='product-price'>Price:</p> <span className="product-price-number">{getCurrencySymbol(currency)}{price}</span>
+ 
+        <button>add to cart</button>
+
+        <p>Longer Description</p>
+        </div>
       </div>
     );
   }
 }
 
-export default ProductPage;
+const mapDispatchToProps = (dispatch) => ({
+  addAttribute: (item) => dispatch(addAttribute(item)),
+});
+
+
+
+const mapStateToProps = ({ currency: { currency } }) => ({
+  currency,
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(ProductPage)
